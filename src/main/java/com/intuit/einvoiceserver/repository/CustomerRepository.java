cad2b99cd018c770fd918eeb3fbb703240fcd971
Mihir Asthana <asthana.mihir@gmail.com>
2017-12-12 21:11:52 -0800

    Added customer repository interface

---
+++ src/main/java/com/intuit/einvoiceserver/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.intuit.einvoiceserver.repository;
+
+import com.intuit.einvoiceserver.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+
+}
