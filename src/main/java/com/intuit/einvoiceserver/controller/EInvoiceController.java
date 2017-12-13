8f0f3ff1fb939f033b9d4d9420094a9334de16ec
Mihir Asthana <asthana.mihir@gmail.com>
2017-12-13 13:56:35 -0800

    Implemented the POST service for invoices

--- src/main/java/com/intuit/einvoiceserver/controller/EInvoiceController.java
+++ src/main/java/com/intuit/einvoiceserver/controller/EInvoiceController.java
@@ -1,35 +1,62 @@
 package com.intuit.einvoiceserver.controller;
 
-import com.intuit.einvoiceserver.model.Customer;
-import com.intuit.einvoiceserver.repository.CustomerRepository;
-import com.intuit.einvoiceserver.model.Invoice;
-import com.intuit.einvoiceserver.repository.InvoiceRepository;
-import com.intuit.einvoiceserver.model.LineItem;
-import com.intuit.einvoiceserver.repository.LineItemRepository;
+import java.util.List;
 
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.List;
+import com.intuit.einvoiceserver.model.Customer;
+import com.intuit.einvoiceserver.model.Invoice;
+import com.intuit.einvoiceserver.model.InvoiceRequest;
+import com.intuit.einvoiceserver.model.LineItem;
+import com.intuit.einvoiceserver.repository.CustomerRepository;
+import com.intuit.einvoiceserver.repository.InvoiceRepository;
+import com.intuit.einvoiceserver.repository.LineItemRepository;
 
+@CrossOrigin
 @RestController
-@RequestMapping("/api/v1.0/")
+@RequestMapping("/api/v1/")
 public class EInvoiceController {
     @Autowired
     InvoiceRepository invoiceRepository;
+    @Autowired
 	LineItemRepository lineItemRepository;
+	@Autowired
+	CustomerRepository customerRepository;
+	
 	@GetMapping("/invoices")
-	public List<Invoice> getAllNotes() {
+	public List<Invoice> getAllInvoices() {
 	    return invoiceRepository.findAll();
 	}
 	
+	
+	//TODO
+	//Rearrange and neat
     @PostMapping("/invoices")
-    public Invoice createInvoice(@Valid @RequestBody Invoice invoice) {
+    public List<LineItem> createInvoice(@Valid @RequestBody InvoiceRequest invoiceRequest) {
+        // service layer
+    	Customer customer = invoiceRequest.getCustomer();
     	
-        return invoiceRepository.save(invoice);
+    	if(!customerRepository.exists((invoiceRequest.getCustomer().getId()))) {
+        	customerRepository.save(invoiceRequest.getCustomer());
+        }
+    	Invoice invoice = invoiceRequest.getInvoice();
+    	invoice.setCustomer(customer);
+    	Long invoiceId = invoiceRepository.saveAndFlush(invoice).getId();
+    	
+    	List<LineItem> lineItems = invoiceRequest.getLineItems();
+    	lineItems.forEach((lI) -> lI.setInvoiceId(invoiceId));
+    	
+    	return lineItemRepository.save(lineItems);
     }
 	
     @GetMapping("/invoices/{id}")
