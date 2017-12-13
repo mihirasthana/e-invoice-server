1855d71584bd12ad033c8ae3e1968aaf41c9cbbf
Mihir Asthana <asthana.mihir@gmail.com>
2017-12-12 21:58:37 -0800

    Updated data models and modifed controller

---
+++ src/main/java/com/intuit/einvoiceserver/controller/EInvoiceController.java
@@ -0,0 +1,13 @@
+package com.intuit.einvoiceserver.controller;
+import com.intuit.einvoiceserver.model.Customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+public class EInvoiceController {
+
+}
