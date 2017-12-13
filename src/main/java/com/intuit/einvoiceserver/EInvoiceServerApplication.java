b9a8b57f710880c56593b6a8527b8fb33a39a645
Mihir Asthana <asthana.mihir@gmail.com>
2017-12-12 20:44:25 -0800

    Initial Commit

---
+++ src/main/java/com/intuit/einvoiceserver/EInvoiceServerApplication.java
@@ -0,0 +1,12 @@
+package com.intuit.einvoiceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInvoiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EInvoiceServerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/intuit/einvoiceserver/model/Invoice.java
@@ -0,0 +1,5 @@
+package com.intuit.einvoiceserver.model;
+
+public class Invoice {
+
+}
