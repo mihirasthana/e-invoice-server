a194192e57d3952ad38f549a59ec3e0eed361126
Mihir Asthana <asthana.mihir@gmail.com>
2017-12-14 00:41:42 -0800

    Added the REST api for typeahead feature

--- src/main/java/com/intuit/einvoiceserver/controller/EInvoiceController.java
+++ src/main/java/com/intuit/einvoiceserver/controller/EInvoiceController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.intuit.einvoiceserver.model.Customer;
@@ -36,6 +37,12 @@ public class EInvoiceController {
 	@GetMapping("/invoices")
 	public List<Invoice> getAllInvoices() {
 	    return invoiceRepository.findAll();
+	}
+	
+	@GetMapping("/customers")
+	public List<String> getAllCustomers(@RequestParam("q") String query) {
+		
+	    return customerRepository.nameStartsWith(query);
 	}
 	
 	
--- src/main/java/com/intuit/einvoiceserver/repository/CustomerRepository.java
+++ src/main/java/com/intuit/einvoiceserver/repository/CustomerRepository.java
@@ -1,10 +1,16 @@
 package com.intuit.einvoiceserver.repository;
 
 import com.intuit.einvoiceserver.model.Customer;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String>{
-
+	//TODO implement limit
+	@Query("select u.name from Customer u where u.name like ?1%")
+	  List<String> nameStartsWith(String name);
 }
